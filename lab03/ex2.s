1049eec347e4decd7fd856af2ce52cb960503402
niuheshui <niuheshui@gmail.com>
2024-04-16 17:59:26 +0800

    lab03

--- lab03/ex2.s
+++ lab03/ex2.s
@@ -21,37 +21,37 @@ dest:
 
 .text
 main:
-    addi t0, x0, 0
-    addi s0, x0, 0
-    la s1, source
-    la s2, dest
+    addi t0, x0, 0      # $t0 = 0 <= k
+    addi s0, x0, 0      # $s0 = 0 <= sum
+    la s1, source       # $s1 = source
+    la s2, dest         # $s2 = dest
 loop:
-    slli s3, t0, 2
-    add t1, s1, s3
-    lw t2, 0(t1)
-    beq t2, x0, exit
-    add a0, x0, t2
-    addi sp, sp, -8
-    sw t0, 0(sp)
-    sw t2, 4(sp)
-    jal square
-    lw t0, 0(sp)
+    slli s3, t0, 2      # $s3 = $t0 * 4 
+    add t1, s1, s3      # $t1 = source + k * 4
+    lw t2, 0(t1)        # $t2 = source[k]
+    beq t2, x0, exit    # if source[k] == 0; goto exit;
+    add a0, x0, t2      # $a0 = souce[k]
+    addi sp, sp, -8      
+    sw t0, 0(sp)        # save callee
+    sw t2, 4(sp)        # save callee
+    jal square          # call function
+    lw t0, 0(sp)         
     lw t2, 4(sp)
     addi sp, sp, 8
-    add t2, x0, a0
-    add t3, s2, s3
-    sw t2, 0(t3)
-    add s0, s0, t2
-    addi t0, t0, 1
-    jal x0, loop
+    add t2, x0, a0      # $t2 = fun(souce[k])
+    add t3, s2, s3      # $t3 = dest + k * 4
+    sw t2, 0(t3)        # dest[k] = $t2
+    add s0, s0, t2      # sum += $t2
+    addi t0, t0, 1      # k++
+    jal x0, loop        # loop
 square:
-    add t0, a0, x0
-    add t1, a0, x0
-    addi t0, t0, 1
-    addi t2, x0, -1
-    mul t1, t1, t2
-    mul a0, t0, t1
-    jr ra
+    add t0, a0, x0      # $t0 = x
+    add t1, a0, x0      # $t1 = x
+    addi t0, t0, 1      # $t0++
+    addi t2, x0, -1     # $t2 <= -1
+    mul t1, t1, t2      # $t1 = -1 * $t1 <= -x
+    mul a0, t0, t1      # $a0 = $t0 * t1 <= -x + (x + 1)
+    jr ra               # ret
 exit:
     add a0, x0, s0
     add a1, x0, x0
--- lab03/factorial.s
+++ lab03/factorial.s
@@ -5,7 +5,7 @@ n: .word 8
 
 .text
 main:
-    la t0, n
+    la t0, n          # $t0 = &n
     lw a0, 0(t0)
     jal ra, factorial
 
@@ -20,5 +20,23 @@ main:
     addi a0, x0, 10
     ecall # Exit
 
+
+# if n == 0 return 1
+# return n * factorial(n - 1)
+
 factorial:
     # YOUR CODE HERE
+    bne a0, x0, else 
+    addi a0, x0, 1
+    jr ra
+else:
+    addi sp, sp, -8
+    sw ra, 4(sp)   # ret address
+    sw a0, 0(sp)    # n
+    addi a0, a0, -1
+    jal ra, factorial 
+    lw t0, 0(sp)
+    lw ra, 4(sp)
+    addi sp, sp, 8
+    mul a0, a0, t0
+    jr ra
